565b73d884b1fc6096c6dfdc64f1e87e82575426
Ali Zarezade <ali.zarezade@gmail.com>
2017-08-29 09:58:13 +0430

    add shaping1 and shaping2

--- activity_shaping.py
+++ activity_shaping.py
@@ -11,10 +11,10 @@ from scipy.linalg import logm, expm
 from scipy.optimize import fsolve, brentq
 import scipy.integrate as integrate
 from event_generation import *
-from activity_maximization import psi
+from activity_maximization import psi, psi_int
 
 
-def f(s, i, tf, alpha, w, d, ell, b):
+def f1(s, i, t0, tf, alpha, w, d, ell, b):
     """
     f_i(s_i) + nu = 0
     sum s_i = c/b
@@ -22,33 +22,48 @@ def f(s, i, tf, alpha, w, d, ell, b):
     n = alpha.shape[0]
     I = np.eye(n)
     g = psi(tf - s, alpha, w)[:, i].dot(d)
-    psi_int = I + alpha.dot(inv(alpha - w * I)).dot(expm((alpha - w * I) * (tf - s)) - expm((alpha - w * I) * tf))
-    g_int = psi_int[:, i].dot(d)
+    _psi_int = I + alpha.dot(inv(alpha - w * I)).dot(expm((alpha - w * I) * (tf - s)) - expm((alpha - w * I) * tf))
+    g_int = _psi_int[:, i].dot(d)
     return g * (b * g_int - ell[i]) * 2 * d[i]
 
 
-def maximize_shaping(b, c, d, ell, t0, tf, mu, alpha, w, tol=1e-1):
+def f2(s, i, t0, tf, alpha, w, d, ell, b):
+    """
+    f_i(s_i) + nu = 0
+    sum s_i = c/b
+    """
+    n = alpha.shape[0]
+    I = np.eye(n)
+    g = psi_int(s, t0, tf, alpha, w)[:, i].dot(d)
+    psi_int_int = I * s - alpha.dot(inv(alpha - w * I)).\
+        dot(inv(alpha - w * I).dot(expm((alpha - w * I) * (tf - s)) + I * s))
+    g_int = psi_int_int[:, i].dot(d)
+    return g * (b * g_int - ell[i]) * 2 * d[i]
+
+
+def maximize_shaping1(b, c, d, ell, t0, tf, mu, alpha, w, tol=1e-1):
     """
     Solve the following optimization:
     """
     n = alpha.shape[0]
     t = np.zeros(n)
 
-    I = np.eye(n)
-    eta = (I + alpha.dot(inv(alpha - w * I)).dot(expm((alpha - w * I) * tf) - I)).dot(mu)
+    # I = np.eye(n)
+    # eta = (I + alpha.dot(inv(alpha - w * I)).dot(expm((alpha - w * I) * tf) - I)).dot(mu)
 
-    ub = max([f(t0, i, tf, alpha, w, d, ell-eta[i], b) for i in range(n)])
-    lb = min([f(tf, i, tf, alpha, w, d, ell-eta[i], b) for i in range(n)])
+    ub = max([f1(t0, i, t0, tf, alpha, w, d, ell, b) for i in range(n)])
+    lb = min([f1(tf, i, t0, tf, alpha, w, d, ell, b) for i in range(n)])
     while abs(sum(t) * b - c) > tol:
         m = (ub + lb) / 2.  # m = -nu
         for i in range(n):
-            if f(t0, i, tf, alpha, w, d, ell-eta[i], b) < m:
+            if f1(t0, i, t0, tf, alpha, w, d, ell, b) < m:
                 t[i] = t0
+            elif f1(tf, i, t0, tf, alpha, w, d, ell, b) > m:
+                t[i] = tf
             else:
-                t[i] = brentq(lambda s: f(s, i, tf, alpha, w, d, ell-eta[i], b) - m, t0, tf)
-                k = i
+                t[i] = brentq(lambda s: f1(s, i, t0, tf, alpha, w, d, ell, b) - m, t0, tf)
         print("ub={:.4f} \t lb={:.4f} \t t_star={} precision={} diff={}".
-              format(ub, lb, [int(t_i) for t_i in t], f(t[k], k, tf, alpha, w, d, ell-eta[i], b) - m, sum(t) * b - c))
+              format(ub, lb, [int(t_i) for t_i in t], f1(t[i], i, t0, tf, alpha, w, d, ell, b) - m, sum(t) * b - c))
         if sum(t) * b > c:
             lb = m
         else:
@@ -56,44 +71,91 @@ def maximize_shaping(b, c, d, ell, t0, tf, mu, alpha, w, tol=1e-1):
     return t
 
 
-def eval_shaping(t, u, d, ell, t0, tf, alpha, w):
+def maximize_shaping2(b, c, d, ell, t0, tf, mu, alpha, w, tol=1e-1):
+    """
+    Solve the following optimization:
+    """
+    n = alpha.shape[0]
+    t = np.zeros(n)
+
+    # I = np.eye(n)
+    # eta = (I + alpha.dot(inv(alpha - w * I)).dot(expm((alpha - w * I) * tf) - I)).dot(mu)
+    # eta_int = np.zeros(n)
+    # for i in range(n):
+    #     eta_int[i] = integrate.quad(lambda s: mu[i] * psi_int(tf - s, alpha, w)[:, i].dot(d), t0, tf)[0]
+
+    ub = max([f2(tf, i, t0, tf, alpha, w, d, ell, b) for i in range(n)])
+    lb = min([f2(t0, i, t0, tf, alpha, w, d, ell, b) for i in range(n)])
+    while abs(sum(t) * b - c) > tol:
+        m = (ub + lb) / 2.  # m = -nu
+        # if m > 0:
+        for i in range(n):
+            t[i] = brentq(lambda s: f2(s, i, t0, tf, alpha, w, d, ell, b) - m, t0, tf)
+        print("ub={:.4f} \t lb={:.4f} \t t_star={} precision={} diff={}".
+              format(ub, lb, [int(t_i) for t_i in t], f2(t[i], i, t0, tf, alpha, w, d, ell, b) - m, sum(t) * b - c))
+        if sum(t) * b > c:
+            ub = m
+        else:
+            lb = m
+    return t
+
+
+def eval_shaping1(t, u, d, ell, t0, tf, alpha, w):
     n = alpha.shape[0]
     integral = np.zeros(n)
     for i in range(n):
         integral[i] = u(t)[i] + integrate.quad(lambda s: u(s)[i] * psi(tf - s, alpha, w)[:, i].dot(d), t0, tf)[0]
-    return np.linalg.norm(integral - ell) ** 2
+    obj = np.linalg.norm(integral - ell)
+    print(integral)
+    print(ell)
+    print(obj)
+    return obj
+
+
+def eval_shaping2(u, d, ell, t0, tf, alpha, w):
+    n = alpha.shape[0]
+    integral = np.zeros(n)
+    for i in range(n):
+        integral[i] = integrate.quad(lambda s: u(s)[i] * psi_int(s, t0, tf, alpha, w)[:, i].dot(d), t0, tf)[0]
+    obj= np.linalg.norm(integral - ell)
+    print(integral)
+    print(ell)
+    print(obj)
+    return obj
 
 
 def main():
-    np.random.seed(0)
+    # np.random.seed(100)
     t0 = 0
     tf = 100
-    n = 8
+    n = 64
     sparsity = 0.3
     mu_max = 0.01
     alpha_max = 0.1
     w = 1
 
-    ell = 10 * np.ones(n)
+    # ell = np.array([2, 2, 2, 4, 4, 4, 4, 4])  # 3 * np.ones(n)
+    ell = 1 * np.random.rand(n)
 
-    b = 100 * mu_max
-    c = 10 * tf * mu_max
+    b = 5 * mu_max
+    c = 20 * tf * mu_max
     d = np.ones(n)
 
     mu, alpha = generate_model(n, sparsity, mu_max, alpha_max)
 
-    t_opt = maximize_shaping(b, c, d, ell, t0, tf, mu, alpha, w)
-    print(eval_shaping(tf, lambda s: [b * (s < t_opt[j]) for j in range(n)], d, ell, t0, tf, alpha, w))
+    t_opt = maximize_shaping1(b, c, d, ell, t0, tf, mu, alpha, w)
+    eval_shaping1(tf, lambda s: [b * (s < t_opt[j]) for j in range(n)], d, ell, t0, tf, alpha, w)
+    # t_opt = maximize_shaping2(b, c, d, ell, t0, tf, mu, alpha, w)
+    # eval_shaping2(lambda s: [b * (s < t_opt[j]) for j in range(n)], d, ell, t0, tf, alpha, w)
 
     # tt = np.arange(t0, tf, 1)
     # yy = np.zeros(len(tt))
     # for i in range(n):
     #     for k in range(len(tt)):
-    #         yy[k] = f(tt[k], i, tf, alpha, w, d, ell, b)
+    #         yy[k] = f1(tt[k], i, t0, tf, alpha, w, d, ell, b)
     #     plt.plot(tt, yy)
     # plt.show()
 
 
 if __name__ == '__main__':
     main()
-
